50193a4acd2b915def2b195d2166c5c1160a7e0d
Kristen M. Thyng <kthyng@gmail.com>
2014-01-10 13:23:41 -0600

    modified tracpy mover in anticipation of integrating tracpy

--- py_gnome/gnome/movers/tracpy_mover.py
+++ py_gnome/gnome/movers/tracpy_mover.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 
 """
-simple_mover.py
+tracpy_mover.py
 
-This is an example mover class -- not really useful, but about as simple as
-can be, for testing and demonstration purposes
-
-It's a steady, uniform current -- one velocity and direction for everywhere
-at all time.
+This calls TRACMASS to step drifters on a structured grid.
 
 
 """
@@ -21,32 +17,28 @@ from gnome import basic_types
 from gnome.movers import Mover
 from gnome.utilities.projections import FlatEarthProjection as proj
 from gnome.utilities import serializable
+from tracpy.tracpy_class import Tracpy
 
 
-class SimpleMover(Mover, serializable.Serializable):
+class TracpyMover(Mover):
 
     """
-    simple_mover
-    
-    a really simple mover -- moves all LEs a constant speed and direction
-    
-    (not all that different than a constant wind mover, now that I think about it)    
+    tracpy_mover
     """
-
-    state = copy.deepcopy(Mover.state)
-    state.add(update=['uncertainty_scale', 'velocity'],
-              create=['uncertainty_scale', 'velocity'])
 
     def __init__(
         self,
-        velocity,
-        uncertainty_scale=0.5,
+        currents_filename,
+        grid_filename=None,
+        nsteps=1, ndays=1, ff=1, tseas=3600.,
+        ah=0., av=0., z0='s', zpar=1, do3d=0, doturb=0, name='test', dostream=0, N=1, 
+        time_units='seconds since 1970-01-01', dtFromTracmass=None, zparuv=None, tseas_use=None
         **kwargs
         ):
         """
-        simple_mover (velocity)
+        tracpy_mover (velocity)
 
-        create a simple_mover instance
+        create a tracpy_mover instance
 
         :param velocity: a (u, v, w) triple -- in meters per second
         
@@ -54,21 +46,23 @@ class SimpleMover(Mover, serializable.Serializable):
         See Mover documentation for remaining valid kwargs.
         """
 
-        self.velocity = np.asarray(velocity,
-                                   dtype=basic_types.mover_type).reshape((3,
-                ))  # use this, to be compatible with whatever we are using for location
-        self.uncertainty_scale = uncertainty_scale
-        super(SimpleMover, self).__init__(**kwargs)
+        # self.velocity = np.asarray(velocity,
+        #                            dtype=basic_types.mover_type).reshape((3,
+        #         ))  # use this, to be compatible with whatever we are using for location
+        # self.uncertainty_scale = uncertainty_scale
+
+        # initializes Tracpy class
+        self.tp = Tracpy(currents_filename, grid_filename=grid_filename, nsteps=nsteps,
+                        ndays=ndays, ff=ff, tseas=tseas, ah=ah, av=av, z0=z0, zpar=zpar,
+                        do3d=do3d, doturb=doturb, name=name, dostream=dostream, N=N,
+                        time_units=time_units, dtFromTracmass=dtFromTracmass,
+                        zparuv=zparuv, tseas_use=tseas_use)
+
+        # calls Mover class
+        super(TracpyMover, self).__init__(**kwargs)
 
     def __repr__(self):
-        return 'SimpleMover(<%s>)' % self.id
-
-    def velocity_to_dict(self):
-        """
-        convert velocity back into a tuple
-        """
-
-        return tuple(self.velocity.tolist())
+        return 'TracpyMover(<%s>)' % self.id
 
     def get_move(
         self,
@@ -94,39 +88,27 @@ class SimpleMover(Mover, serializable.Serializable):
         # Get the data:
 
         try:
-            positions = spill['positions']
+            positions = spill['positions'] # Nx3 with lon,lat,z
             status_codes = spill['status_codes']
         except KeyError, err:
             raise ValueError('The spill does not have the required data arrays\n'
                               + err.message)
 
         # which ones should we move?
-
+        # status codes for things like off map or on beach
         in_water_mask = status_codes == basic_types.oil_status.in_water
 
         # compute the move
+        # (need to subtract in tracpy to get differential position)
 
         delta = np.zeros_like(positions)
 
         if self.active and self.on:
-            delta[in_water_mask] = self.velocity * time_step
 
-            # add some random stuff if uncertainty is on
+            # CALL TRACPY HERE
+            # delta[in_water_mask] = tracpy. #self.velocity * time_step
 
-            if spill.uncertain:
-                num = sum(in_water_mask)
-                scale = self.uncertainty_scale * self.velocity \
-                    * time_step
-                delta[in_water_mask, 0] += random.uniform(-scale[0],
-                        scale[0], num)
-                delta[in_water_mask, 1] += random.uniform(-scale[1],
-                        scale[1], num)
-                delta[in_water_mask, 2] += random.uniform(-scale[2],
-                        scale[2], num)
-
-            # scale for projection
-
-            delta = proj.meters_to_lonlat(delta, positions)  # just the lat-lon...
+            # NEED TO HAVE ALREADY CONVERTED TO LAT LON HERE FOR DELTA LON/LAT
 
         return delta
 
